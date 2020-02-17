20ec6a8e51e55fb7c658f41f802fe7c453c4d465
Alison PLC <alisonpietro@gmail.com>
2020-02-17 10:56:10 -0300

    2020.02.17.#70

--- src/socialCard.js
+++ src/socialCard.js
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom";
 import "./scss/main.scss";
 import dev from "./Images/dev.svg";
 import WebFont from "webfontloader";
+import UserIcon from "./Images/UserIcon.png";
+
 WebFont.load({
   google: {
     families: ["Asap Web:300,400,700", "sans-serif"]
@@ -14,15 +16,19 @@ const SocialCard = () => {
       <div className="socialcard-Logo-Container">
         <img src={dev} alt="DEV_COOL_LOGO" />
       </div>
-      <h2 className="socialcard-main-topic">Learning React? Start Small.</h2>
-      <div className="socialcard-credits">
-        <img src="null" alt="Dave Ceddia" />
-        <p>
-          <a href="#">@dcedia</a>
-        </p>
+      <h2 className="socialcard-Main-Topic">Learning React? Start Small.</h2>
+      <div className="socialcard-Person">
+        
+          <h3 className="person-Name">Dave Ceddia</h3>
+        <div className="person-Icon-Container">
+
+        <img classname="person-Icon" src={UserIcon} alt="Dave Ceddia" />
+        </div>
+        
       </div>
+      <a className="socialcard-Link"  href="#">@dcedia</a>
     </div>
   );
 };
-
+/* */
 export default SocialCard;
